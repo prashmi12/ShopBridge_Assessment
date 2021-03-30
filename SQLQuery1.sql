f88d38c8d0895a10a854cbc464129f0cfeb0e07a
prashmi12 <prashmi.06@gmail.com>
2021-03-30 13:09:42 +0530

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Product](  
+    [ProductId] [int] PRIMARY KEY IDENTITY(1,1) NOT NULL,  
+    [ProductName] [nvarchar](100) NOT NULL,  
+    [ProductDescription] [varchar] NOT NULL,  
+    [Price] [int] NOT NULL) 
